45395f4361fcff50c26464d63fa0a10b38d0e6b0
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-02-09 01:42:04 +0900

    chore: 不要な設定の削除

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :discord, ENV["DISCORD_CLIENT_ID"], ENV["DISCORD_CLIENT_SECRET"], scope: "identify email"
 end
-
-OmniAuth.config.allowed_request_methods = [:post, :get]
