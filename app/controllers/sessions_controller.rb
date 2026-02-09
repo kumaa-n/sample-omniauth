2a08f5cb169671eeb9576f1ab7f589f7c04a3d75
kumaa-n <205625192+kumaa-n@users.noreply.github.com>
2026-02-09 14:42:53 +0900

    fix: 認証失敗時のロジックを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,21 +10,33 @@ class SessionsController < ApplicationController
     if authentication
       user = authentication.user
     else
-      user = User.find_or_create_by!(email: auth.info.email)
-
-      user.authentications.create!(
-        provider: auth.provider,
-        uid: auth.uid,
-        name: auth.info.name
-      )
+      begin
+        ActiveRecord::Base.transaction do
+          user = User.find_or_create_by!(email: auth.info.email)
+          user.authentications.create!(
+            provider: auth.provider,
+            uid: auth.uid,
+            name: auth.info.name
+          )
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        redirect_to root_path, alert: "ログインに失敗しました。"
+        return
+      end
     end
 
+    reset_session
     session[:user_id] = user.id
+    session[:authentication_id] = authentication&.id || user.authentications.find_by(provider: auth.provider, uid: auth.uid)&.id
     redirect_to users_path, notice: "ログインしました。"
   end
 
+  def omniauth_failure
+    redirect_to root_path, alert: "認証に失敗しました。"
+  end
+
   def destroy
-    session[:user_id] = nil
+    reset_session
     redirect_to root_path, notice: "ログアウトしました。"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: %i[show]
+
   def show
     @user = current_user
-    redirect_to root_path unless @user
+    @current_authentication = @user.authentications.find_by(id: session[:authentication_id])
   end
 end
